65bd7b9973d539d264ec315c68196c9a856c85be
dazhu04@gmail.com <dazhu04@gmail.com>
2021-04-03 11:35:47 +0300

    Initial commit

---
+++ RandomSponegbobQuotes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace RandomSponegbobQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string location = @"C:\Users\Lenovo\Desktop\Programming Projects\Visual Studio Projects\C# Projects\Console Projects\RandomSponegbobQuotes\RandomSponegbobQuotes\spongebob.txt";
+
+            ArrayList quotes = new ArrayList();
+
+            foreach (string line in File.ReadAllLines(location))
+            {
+                quotes.Add(line);
+            }
+
+            Random rand = new Random();
+
+            while (true)
+            {
+                Console.WriteLine(quotes[rand.Next(quotes.Count)]);
+                Console.WriteLine("Enter y if you want to see another random quote: ");
+
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() != "y")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
